b62c3f2ece31656abe4e9301b1b024b5b0b0c342
cloudsbelow <150115403+cloudsbelow@users.noreply.github.com>
2025-03-23 23:27:04 -0700

    lol

--- Source/TemplateStuff/templates/templateZipmover.cs
+++ Source/TemplateStuff/templates/templateZipmover.cs
@@ -22,7 +22,7 @@ public class TemplateZipmover:Template{
   SplineAccessor spos;
   EntityData dat;
   Vector2 offset;
-  public TemplateZipmover(EntityData d, Vector2 offset):this(d,offset,0){}
+  public TemplateZipmover(EntityData d, Vector2 offset):this(d,offset,d.Int("depthoffset",0)){}
   public TemplateZipmover(EntityData d, Vector2 offset, int depthoffset)
   :base(d.Attr("template",""),d.Position+offset,depthoffset){
     Add(new Coroutine(Sequence()));
