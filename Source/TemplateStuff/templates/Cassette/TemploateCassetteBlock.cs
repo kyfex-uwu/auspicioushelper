62b989d0b6475199391414122dc92a9b94706070
cloudsbelow <150115403+cloudsbelow@users.noreply.github.com>
2025-06-10 03:20:35 -0700

    ack

--- Source/TemplateStuff/templates/Cassette/TemploateCassetteBlock.cs
+++ Source/TemplateStuff/templates/Cassette/TemploateCassetteBlock.cs
@@ -63,7 +63,7 @@ public class TemplateCassetteBlock:TemplateDisappearer, IMaterialObject, IChanne
   }
   public override bool enforceAsVis {get{
     bool inlayer = CassetteMaterialLayer.layers.TryGetValue(channel,out var layer);
-    return base.enforceAsVis && !inlayer || layer.fg==null;
+    return base.enforceAsVis && (!inlayer || layer.fg==null);
   }}
   public void tryManifest(){
     Player p = Scene?.Tracker.GetEntity<Player>();
